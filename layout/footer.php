499950d4abf1d7682d11df0fdb91770ca4c93bd3
fahmid <dnetfahmidhasan@gmail.com>
2024-07-15 16:29:01 +0600

    initial commit

---
+++ layout/footer.php
@@ -0,0 +1,29 @@
+  <!-- Core JS -->
+  <!-- build:js assets/vendor/js/core.js -->
+  
+  <script src="js/jquery.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="js/perfect-scrollbar.js"></script>
+  <script src="js/hammer.js"></script>
+  <!--<script src="js/i18n.js"></script>-->
+  <script src="js/typeahead.js"></script>
+  <script src="js/menu.js"></script>
+  
+  <!-- endbuild -->
+
+  <!-- Vendors JS -->
+  <script src="js/apexcharts.js"></script>
+
+  <!-- Main JS -->
+  <script src="js/main.js"></script>
+  
+
+  <!-- Page JS -->
+  <script src="js/dashboards-crm.js"></script>
+  
+</body>
+
+</html>
+
+<!-- beautify ignore:end -->
